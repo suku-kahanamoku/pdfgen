2de84821e226c2e6be8ba39b1260efcb0528c849
Suku <sukusovi@gmail.com>
2026-05-05 20:00:28 +0200

    Update.

--- helper.php
+++ helper.php
@@ -49,7 +49,7 @@ function EMIT_MARKER(): void
     if (!empty($GLOBALS['display_marker']) && isset($GLOBALS['_marker'])) {
         $id = $GLOBALS['_marker'];
         unset($GLOBALS['_marker']);
-        echo '<div style="font-size:8px;color:#000;line-height:1;margin:0;padding:0;">TOCMARKER_' . $id . '</div>';
+        echo '<div style="position:absolute;font-size:8px;color:#000;line-height:1;margin:0;padding:0;">TOCMARKER_' . $id . '</div>';
     }
 }
 
